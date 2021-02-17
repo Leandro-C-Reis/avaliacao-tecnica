80c1209d295d18ff31179468e1af161b269fc6d8
Leandro Christian dos Reis <leandroreis02@gmail.com>
2021-02-17 11:52:49 -0300

    New dependency added

--- frontend/src/page/App.js
+++ frontend/src/page/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
 
-const App = () => (
-    <div className="blue-text text-darken-2" >Hello World</div>
-);
+const App = () => {
+    return <></>;
+}
 
 export default App;
