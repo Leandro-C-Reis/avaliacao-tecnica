6979720137f8bdfc7b3beba6eac68fba4df2558c
Leandro Christian dos Reis <leandroreis02@gmail.com>
2021-02-17 10:03:05 -0300

    New dependencies and source format

---
+++ frontend/src/page/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+
+const App = () => (
+    <div className="blue-text text-darken-2" >Hello World</div>
+);
+
+export default App;
